dadecd5b8aa19348b4ad1c61331e48cce64fb88a
Shoma Nishitateno <shomatan@users.noreply.github.com>
2016-09-07 11:34:48 +0900

    Install build packages.

--- init.sh
+++ init.sh
@@ -54,6 +54,7 @@ echo "  => nmap..." && yum -y install nmap > /dev/null
 echo "  => zsh...." && yum -y install zsh  > /dev/null
 echo "  => htop..." && yum -y install htop --enablerepo=epel > /dev/null
 echo "  => screen..." && yum -y install screen > /dev/null
+echo "  => build packages..." && yum -y install libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip > /dev/null
 echo
 
 echo "=> Cron package"
